48bebb2022f59107b4910a8f065af673de1dfd59
esantofimio <esantofimios@unal.edu.co>
2018-03-03 23:34:43 -0500

    Finalizando Juego

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,21 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component{
-/* constructor(props){
- super(props);
- this.state = {
-  value: null,
- };
-}*/
+function Square (props){
 
-  render(){
     return(
-      <button className = "square" onClick = {() => this.props.onClick()}> 
-      {this.props.value}
+      <button className = "square" onClick = {props.onClick}> 
+      {props.value}
       </button>
     );
-  }
+  
 
 }
 
@@ -25,14 +18,21 @@ class Board extends React.Component{
   super(props);
   this.state = {
     squares: Array(9).fill(null),
+    xIsNext: true,
   };
 }
 
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    if(calculateWinner(squares) || squares[i]){
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : '0';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
 
 
   }
@@ -46,7 +46,18 @@ class Board extends React.Component{
   }
 
   render(){
-    const status = 'Next player: X';
+
+    const winner = calculateWinner(this.state.squares);
+    let status;
+
+    if(winner){
+      status = 'Winner: ' + winner;
+
+    }else{
+
+      status = 'Next player ' + (this.state.xIsNext ? 'X' : '0');
+
+    }
 
     return(
       <div>
@@ -85,6 +96,27 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++){
+    const [a, b, c] = lines[i]
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // =========
